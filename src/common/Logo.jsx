fbedf8b37f97d8af98aa1f2564ffa50f7b0e2b01
fahad <fk336733@gmail.com>
2025-06-24 13:27:00 +0500

    making cards adjust size

--- src/common/Logo.jsx
+++ src/common/Logo.jsx
@@ -2,7 +2,7 @@ export const Logo = () => {
 
 
     return (
-        <span className={" flex text-xl md:text-[32px] font-bold  cFont tracking-normal  cursor-pointer   "}> TRUE CLASSIC </span>
+        <span className={" flex text-2xl md:text-[32px] font-bold  cFont tracking-normal  cursor-pointer   "}> TRUE CLASSIC </span>
     )
 
 }
