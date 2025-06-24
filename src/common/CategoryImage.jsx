150a7f8952f82292692bdcc35af6c1165508fa5c
fahad <fk336733@gmail.com>
2025-06-24 11:39:10 +0500

    making cards adjust size

--- src/common/CategoryImage.jsx
+++ src/common/CategoryImage.jsx
@@ -1,8 +1,8 @@
 export  const CategoryImage =({imgUrl,label})=>{
 
     return (
-        <div className={' rounded-lg text-center hover:underline  '}>
-            <img className={'max-w-[226px] max-h-[290px] rounded-lg hover:scale-[101%] flex'} src={imgUrl} alt={label}/>
+        <div className={'flex px-2 flex-col rounded-lg text-center hover:underline  '}>
+            <img className={'max-w-[226px]  max-h-[290px]  rounded-lg hover:scale-[101%] flex'} src={imgUrl} alt={label}/>
             <label className={'staff-condensed '}>{label}</label>
 
         </div>
